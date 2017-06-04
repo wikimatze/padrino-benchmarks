fcb1c2a26563790ffa0ff7233bfc01c77eae4c6f
Matthias Guenther <matthias@wikimatze.de>
2017-06-04 15:07:51 +0200

    Add renderer

---
+++ renderer/erb-app/app/helpers/render_helper.rb
@@ -0,0 +1,13 @@
+# Helper methods defined here can be accessed in any controller or view in the application
+
+module ErbApp
+  class App
+    module RenderHelper
+      # def simple_helper_method
+      # ...
+      # end
+    end
+
+    helpers RenderHelper
+  end
+end
---
+++ renderer/liquid-app/app/helpers/render_helper.rb
@@ -0,0 +1,13 @@
+# Helper methods defined here can be accessed in any controller or view in the application
+
+module LiquidApp
+  class App
+    module RenderHelper
+      # def simple_helper_method
+      # ...
+      # end
+    end
+
+    helpers RenderHelper
+  end
+end
